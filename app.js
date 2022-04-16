261c05b83aa33b80c857c43327ea67c84d846318
barath-7 <79738124+barath-7@users.noreply.github.com>
2022-04-16 21:53:06 +0530

    Added code to get the location from command line

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
-require('dotenv').config()
-const request = require('postman-request');
-const {URL,GEOCODE_URL} = process.env
+const getGeocode = require("./geoCode");
+const getWeather = require("./weaterStack");
+let address = process.argv[2];
+if (!address) {
+  console.log(
+    'Enter a valid location in the command line argument. For Example << node app.js "London" >>'
+  );
+} else {
+  getGeocode("California", (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
 
-
-request({url:URL,json:true},(err,res)=>{
-  let data = res.body
-  if(err){
-    console.log('Unable to access weather api')
-    return
-  }
-  if(data.error){
-    console.log('Unable to find location')
-    return
-  }
-    
-    console.log(
-      `The weather today is ${data.current.weather_descriptions[0]}. 
-      It is currently ${data.current.temperature} degrees out there. But it feels more like ${data.current.feelslike} degrees. 
-      There is ${data.current.precip}% chance of rain.`
-    );
-})
-
-request({url:GEOCODE_URL,json:true},(err,res)=>{
-
-  let data = res.body
-  if(err){
-    console.log('Unable connect to geocoding api')
-    return
-  }
-  if(data.features.length===0){
-    console.log('Unable to find the latitude and longitude')
-    return
-  }
-  let placeName = data.features[0].place_name
-  let latitude = data.features[0].center[1]
-  let longitude = data.features[0].center[0]
-  console.log(`The coordinates of ${placeName} is ${latitude}, ${longitude}`)
-})
+    getWeather(data.latitude, data.longitude, (error, weatherData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(weatherData);
+    });
+  });
+}
